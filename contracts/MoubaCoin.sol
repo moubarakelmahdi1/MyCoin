c148e41e0c39844a01d07fae8b274aaee2e3ac4a
"moubarakelmahdi1" <"moubarakelmahdi1@gmail.com">
2022-04-08 04:08:45 +0200

    EEM My first Token

---
+++ contracts/MoubaCoin.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.7.4;
+
+contract MoubaCoin {
+
+    address public minter;
+
+    mapping(address => uint) public balances;
+
+    event Sent(address from, address to, uint amount);
+
+
+    constructor() {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        require(amount <= balances[msg.sender], "Insufficient Balance");
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver,amount);
+    }
+}
